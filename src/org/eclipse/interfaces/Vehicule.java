e123688463dff1a3358827b258a9df606e3b7600
Mehdi B <kyo.alone06@gmail.com>
2022-06-10 15:40:48 +0200

    first commit

---
+++ src/org/eclipse/interfaces/Vehicule.java
@@ -0,0 +1,10 @@
+package org.eclipse.interfaces;
+
+// Dans une FunctionalInterface, nous en retrouverons qu'un seule 
+// methode abstract
+@FunctionalInterface
+public interface Vehicule {
+	
+	void rouler(String type);
+
+}
